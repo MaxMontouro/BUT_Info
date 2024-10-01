65c0481f3e6b38ed9e63f627ce26b5fe7d47aca4
MaxMontouro <max.montouro@gmail.com>
2024-10-01 11:25:54 +0200

    -ajout d'un nouveau test qui valide si la liste est vide quand on donne 1 en paramètre

--- Annee3/S5/R5_D_07_Qualite_dev/td2-tp2/junit5-jupiter-starter-gradle/src/test/java/com/Maks/facteursPremiers/FacteursPremiersTest.java
+++ Annee3/S5/R5_D_07_Qualite_dev/td2-tp2/junit5-jupiter-starter-gradle/src/test/java/com/Maks/facteursPremiers/FacteursPremiersTest.java
@@ -13,10 +13,9 @@ class FacteursPremiersTest {
     @Test
     public void generate_devrait_retourner_la_decomposition_du_nombre(){
         //GIVEN
-        int exemple = 6;
+        int exemple = 2;
         List<Integer> listeEntiers = new ArrayList<>();
         listeEntiers.add(2);
-        listeEntiers.add(3);
 
         //WHEN
         List<Integer> array = new ArrayList();
@@ -24,5 +23,19 @@ class FacteursPremiersTest {
 
         //THEN
         assertEquals(array, listeEntiers);
-    };
+    }
+
+    @Test
+    public void generate_devrait_renvoyer_liste_vide(){
+        //GIVEN
+        int exemple = 1;
+        List<Integer> listeEntiers = new ArrayList<>();
+
+        //WHEN
+        List<Integer> array = new ArrayList();
+        array = FacteursPremiers.generate(exemple);
+
+        //THEN
+        assertEquals(array, listeEntiers);
+    }
 }
